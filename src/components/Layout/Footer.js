28debf1f6994084287e181e5dd7341212c7a3037
ltb <liangtb@qq.com>
2020-05-01 15:42:04 +0800

    feat: 部分完成footer

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,103 @@
+import React, { PureComponent } from "react";
+import { Grid, Popper, Paper } from "@material-ui/core";
+import styles from "./index.module.css";
+import weChat155 from "assets/wechat-155.jpeg";
+import { Link } from "react-router-dom";
+
+export default class extends PureComponent {
+    state = {
+        open: false,
+        anchorEl: null
+    };
+
+    showWeChat = e => {
+        this.setState({ anchorEl: e.currentTarget, open: true });
+    };
+
+    closeWeChat = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { open, anchorEl } = this.state;
+
+        return (
+            <footer className={styles.footerBg}>
+                <Popper open={open} anchorEl={anchorEl} placement="right">
+                    <Paper style={{ padding: 5 }}>
+                        <img src={weChat155} height="215" alt="weChat" />
+                    </Paper>
+                </Popper>
+                <div className={styles.footer}>
+                    <Grid container>
+                        <Grid item xs={12} sm={5}>
+                            <p className={styles.title}>联系我们</p>
+                            <ul>
+                                <li>
+                                    客户咨询专线：15521312099（微信同号）
+                                    <i
+                                        className="iconfont"
+                                        onMouseEnter={this.showWeChat}
+                                        onMouseLeave={this.closeWeChat}
+                                    >
+                                        &#xe7e5;
+                                    </i>
+                                </li>
+                                <li>
+                                    技术支持电话：15521312099（微信同号）
+                                    <i
+                                        className="iconfont"
+                                        onMouseEnter={this.showWeChat}
+                                        onMouseLeave={this.closeWeChat}
+                                    >
+                                        &#xe7e5;
+                                    </i>
+                                </li>
+                                <li>技术支持QQ：452391310</li>
+                                <li>开发者QQ群：23831587</li>
+                                <li>
+                                    技术支持邮箱：
+                                    <a href="mailto:help@onlyid.net">help@onlyid.net</a>
+                                </li>
+                            </ul>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <p className={styles.title}>开发者服务</p>
+                            <ul>
+                                <li>
+                                    <Link to="/docs">开发接入文档</Link>
+                                </li>
+                                <li>
+                                    <Link to="/prices">套餐价格和功能</Link>
+                                </li>
+                                <li>
+                                    <Link to="/docs/faq">常见问题 FAQ</Link>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/onlyid">GitHub 仓库</a>
+                                </li>
+                            </ul>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <p className={styles.title}>关于唯ID</p>
+                            <ul>
+                                <li>
+                                    <Link to="/about">唯ID介绍（团队介绍）</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about#join">加入我们（工作机会）</Link>
+                                </li>
+                                <li>
+                                    <Link to="/user-agreement">用户协议</Link>
+                                </li>
+                                <li>
+                                    <Link to="/privacy-policy">隐私权政策</Link>
+                                </li>
+                            </ul>
+                        </Grid>
+                    </Grid>
+                </div>
+            </footer>
+        );
+    }
+}
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,7 @@ import styles from "./index.module.css";
 import Header from "./Header";
 import HeaderM from "./HeaderM";
 import { Hidden } from "@material-ui/core";
+import Footer from "./Footer";
 
 class Layout extends PureComponent {
     render() {
@@ -17,11 +18,7 @@ class Layout extends PureComponent {
                     <Header />
                 </Hidden>
                 <div className={styles.content}>{children}</div>
-                <div className={styles.footer}>
-                    &copy; 2015 - {new Date().getFullYear()}
-                    <span style={{ marginLeft: 20, marginRight: 20 }}>深圳市友全科技有限公司</span>
-                    All rights reserved.
-                </div>
+                <Footer />
             </div>
         );
     }
